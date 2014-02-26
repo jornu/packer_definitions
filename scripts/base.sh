c9da9ad5ddb76323379cf009685d817dc1f8fdcf
Jorge Núñez <jorgeng87@gmail.com>
2014-02-27 00:45:42 +0100

    Almost finished

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,5 +6,8 @@ apt-get clean
 # Needed to avoid "ERROR: The certificate of `apt.puppetlabs.com' is not trusted." later
 apt-get -y install ca-certificates
 
+# Needed by "vagrant-vbguest" plugin to install the guest additions
+apt-get -y install bzip2
+
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
--- scripts/vmadditions.sh
+++ scripts/vmadditions.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-if [ $PACKER_BUILDER_TYPE == 'virtualbox' ]; then
-    echo "Installing VirtualBox guest additions"
+echo "Installing VirtualBox guest additions"
 
-    apt-get install -y linux-headers-$(uname -r) build-essential perl
-    apt-get install -y dkms
+apt-get install -y linux-headers-$(uname -r) build-essential perl
+apt-get install -y dkms
 
-    VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-    mount -o loop /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run --nox11
-    umount /mnt
-    rm /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso
-fi
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run --nox11
+umount /mnt
+rm /home/vagrant/VBoxGuestAdditions.iso
